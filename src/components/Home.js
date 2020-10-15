b44c257a66530c2172340a7090f7665d21968651
rohit20001221 <rohit20001221@gmail.com>
2020-10-15 14:25:12 +0530

    removed history api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Button } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
-  const history = useHistory();
+  // const history = useHistory();
   const roomID = useRef();
+  const [disabled, setDisabled] = useState(false);
 
   const joinRoom = (event) => {
     event.preventDefault();
-    history.replace(`/room/${roomID.current.value}`);
+    setDisabled(true);
+    window.location.href = `/room/${roomID.current.value}`;
   };
 
   const createRoom = () => {
+    setDisabled(true);
     fetch("https://rocky-woodland-05850.herokuapp.com/createRoom")
       .then((res) => res.json())
       .then((roomInfo) => {
-        history.replace(`/room/${roomInfo.roomID}`);
+        window.location.href = `/room/${roomInfo.roomID}`;
       });
   };
 
   return (
     <div className="home">
       <div className="home__createRoom">
-        <Button onClick={createRoom}>Create Room</Button>
+        <Button disabled={disabled} onClick={createRoom}>Create Room</Button>
       </div>
       <div className="home__joinRoom">
         <form onSubmit={joinRoom}>
           <input ref={roomID} placeholder="Enter room ID" type="text" />
           {/* <input placeholder="Enter your name" type="text" /> */}
-          <Button type="submit">Join Room</Button>
+          <Button disabled={disabled} type="submit">Join Room</Button>
         </form>
       </div>
     </div>
